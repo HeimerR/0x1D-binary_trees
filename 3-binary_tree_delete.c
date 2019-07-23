651e223632d71d027c3436cb7a38827b2b2fdd22
Heimer <732@holbertonschool.com>
2019-07-23 00:17:49 +0000

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,12 +3,15 @@
  * binary_tree_delete - deletes a binary tree
  * @tree: pointer to root
  * Return: no return
-**/
+ **/
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->left)
-		binary_tree_delete(tree->left);
-	if (tree->right)
-		binary_tree_delete(tree->right);
-	free(tree);
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_delete(tree->left);
+		if (tree->right)
+			binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
