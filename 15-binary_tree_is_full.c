850153416a14710748cf5e6907759732be1d77c3
Heimer <732@holbertonschool.com>
2019-07-23 16:36:49 +0000

    task 15 first attempt

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the node
+ * Return: 1 if it is full 0 otherwise
+ **/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int tmp1, tmp2;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if ((!tree->left && tree->right) || (!tree->right && tree->left))
+		return (0);
+	//if ((!tree->left && !tree->right) || (tree->right && tree->left))
+	//	return (1);
+	if (tree->right && tree->left)
+	{
+		tmp1 = binary_tree_is_full(tree->left);
+		tmp2 = binary_tree_is_full(tree->right);
+		return (tmp1 || tmp2);
+	}
+}
