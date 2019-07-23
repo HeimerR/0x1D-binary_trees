8813f5eb634f634d61b77c325427fa239cab47ab
Heimer <732@holbertonschool.com>
2019-07-23 00:14:58 +0000

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,14 +6,9 @@
 **/
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree->left && !tree->right)
-	{
-		free(tree);
-		return;
-	}
-	else
-	{
+	if (tree->left)
 		binary_tree_delete(tree->left);
+	if (tree->right)
 		binary_tree_delete(tree->right);
-	}
+	free(tree);
 }
