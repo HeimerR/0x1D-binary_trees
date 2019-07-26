44878fab5cf1caa60c1fbd2c50ccbf9b59cff827
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:53:59 +0000

    Fix Issue Var G

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the node
- * Return: height of the tree
+ * Return: height of the tree from node
  **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
