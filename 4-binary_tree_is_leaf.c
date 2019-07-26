a6767484b97c36da67d6c68ad10f88d9adaa0cd3
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:48:18 +0000

    Fix Issue Var G

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to node to check
+ * @node: pointer to node to verify is a leaf
  * Return: 1 if node is a leaf, otherwise 0
  **/
 int binary_tree_is_leaf(const binary_tree_t *node)
