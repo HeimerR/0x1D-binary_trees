3398a4595ed0ca68441a68e7ff1c95b012c78941
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:44:32 +0000

    Fix Issue Var G

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_insert_left - inserts a node in the binary tree (left)
  * @parent: pointer to a parent node
- * @value: number value in the node
+ * @value: number value in the node inserted
  * Return: pointer to the new node
 **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
