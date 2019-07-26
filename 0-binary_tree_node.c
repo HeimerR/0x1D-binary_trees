bc1e84705cce6eb7c1a2c8aa3b001356fc336ed7
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:42:18 +0000

    Fix Issue Var G

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
  * binary_tree_node - creates a node in the binary tree
  * @parent: pointer to a parent node
  * @value: number value in the node
- * Return: pointer to de new node
+ * Return: pointer to the new node
 **/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
