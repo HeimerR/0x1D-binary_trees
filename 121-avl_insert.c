439df3985384ed7fb8dd1ce4807d4d8fa01869fe
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:23:52 +0000

    Fix Issue Var G

---
+++ 121-avl_insert.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+//#include "0-binary_tree_node.c"
+//#include "103-binary_tree_rotate_left.c"
+//#include "104-binary_tree_rotate_right.c"
+//#include "14-binary_tree_balance.c"
+/**
+ * avl_insert - Insert a value in an AVL tree
+ * @tree: pointer to the root
+ * @value: value to insert
+ * Return: pointer to new node created
+ **/
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *tmp, *new;
+	int flag;
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return(*tree);
+	}
+	tmp = *tree;
+	flag = 0;
+	while (flag == 0)
+	{
+		if (value > tmp->n)
+			if (tmp->right)
+				tmp = tmp->right;
+			else
+			{
+				flag = 1;
+				new = binary_tree_node(tmp, value);
+				tmp->right = new;
+				return(new);
+			}
+		else
+			if (tmp->left)
+				tmp = tmp->left;
+			else
+			{
+				flag = 1;
+				new = binary_tree_node(tmp, value);
+				tmp->left = new;
+				return(new);
+			}
+	}
+	return(NULL);
+}
