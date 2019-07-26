1881091d3de3e57dca1c872d6fb42405b28dd90e
Giovanny Perez <789@holbertonschool.com>
2019-07-26 20:20:53 +0000

    Fix Issue Var G

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node
- * Return: pointer to uncle
+ * Return: pointer to uncle found
  **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
