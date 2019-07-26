1e8377b80b8e515dd6b5b49c3e17d6f66b98db1a
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:45:29 +0000

    Fix Issue Var G

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,7 +3,7 @@
  * binary_tree_insert_right - inserts a node in the binary tree (right)
  * @parent: pointer to a parent node
  * @value: number value in the node
- * Return: pointer to the new node
+ * Return: pointer to the new node inserted
 **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
