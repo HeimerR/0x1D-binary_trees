3c293dd4315fd0d8e135e85448c4f2a7b6aa09f2
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:56:52 +0000

    Fix Issue Var G

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to the node
- * Return: size of the tree
+ * Return: size of the tree from the node
  **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
