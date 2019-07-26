44dd668eb4463509cb999f870afe25ef7ff47f2f
Giovanny Perez <789@holbertonschool.com>
2019-07-26 19:49:08 +0000

    Fix Issue Var G

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_root - checks if a node is a rootf
- * @node: pointer to node to check
+ * binary_tree_is_root - checks if a node is a root
+ * @node: pointer to node to check if is root
  * Return: return 1 if node is a root, otherwise 0
  **/
 int binary_tree_is_root(const binary_tree_t *node)
